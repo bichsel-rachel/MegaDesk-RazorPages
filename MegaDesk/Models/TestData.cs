7c3bb17856940beadf77ba1aecfc7a1c4f5530c6
Scott Mosher <scott@scottmosherphotography.com>
2020-02-23 15:34:10 -0700

    Added seed data

---
+++ MegaDesk/Models/TestData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MegaDesk.Data;
+
+namespace MegaDesk.Models
+{
+    public class TestData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MegaDeskContext(
+                    serviceProvider.GetRequiredService<DbContextOptions<MegaDeskContext>>())) 
+                {
+
+                    // If the database already has 1 or more records, skipping seeding the DB
+                    if (context.Order.Any())
+                    {
+                        return;
+                    }
+
+                // If it is empty, then lets add in some orders that we can play with
+                context.Order.AddRange(
+                    new Order
+                    {
+                        FirstName = "Floor",
+                        LastName = "Jansen",
+                        Width = 35,
+                        Depth = 48,
+                        Drawers = 5,
+                        // TODO: Change this to "Oak"
+                        SurfaceMaterial = 2,
+                        RushOrder = 3,
+                        DateAdded = DateTime.Parse("2020-1-05")
+                    },
+
+                    new Order
+                    {
+                        FirstName = "Gavin",
+                        LastName = "Rossdale",
+                        Width = 40,
+                        Depth = 25,
+                        Drawers = 1,
+                        // TODO: Change this to "Veneer"
+                        SurfaceMaterial = 3,
+                        RushOrder = 5,
+                        DateAdded = DateTime.Parse("2020-1-07")
+                    },
+
+                    new Order
+                    {
+                        FirstName = "Billy",
+                        LastName = "Corgan",
+                        Width = 25,
+                        Depth = 13,
+                        Drawers = 9,
+                        // TODO: Change this to "Pine"
+                        SurfaceMaterial = 4,
+                        RushOrder = 14,
+                        DateAdded = DateTime.Parse("2020-1-10")
+                    },
+
+                    new Order
+                    {
+                        FirstName = "James",
+                        LastName = "Hetfield",
+                        Width = 30,
+                        Depth = 40,
+                        Drawers = 7,
+                        // TODO: Change this to "Laminate"
+                        SurfaceMaterial = 2,
+                        RushOrder = 3,
+                        DateAdded = DateTime.Parse("2020-1-15")
+                    },
+
+                    new Order
+                    {
+                        FirstName = "Robert",
+                        LastName = "Plant",
+                        Width = 80,
+                        Depth = 35,
+                        Drawers = 2,
+                        // TODO: Change this to "Rosewood"
+                        SurfaceMaterial = 1,
+                        RushOrder = 5,
+                        DateAdded = DateTime.Parse("2020-1-25")
+                    },
+
+                    new Order
+                    {
+                        FirstName = "Gwen",
+                        LastName = "Stefani",
+                        Width = 35,
+                        Depth = 41,
+                        Drawers = 5,
+                        // TODO: Change this to "Veneer"
+                        SurfaceMaterial = 4,
+                        RushOrder = 3,
+                        DateAdded = DateTime.Parse("2020-2-05")
+                    }
+
+
+                    ) ;
+
+                context.SaveChanges();
+                }
+        }
+    }
+}
--- MegaDesk/Program.cs
+++ MegaDesk/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MegaDesk.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MegaDesk
 {
@@ -13,7 +15,28 @@ namespace MegaDesk
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Add the code for seeding the DB if it is empty
+
+            // Need Microsoft.Extensions.DependencyInjection for this to work
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    TestData.Initialize(services);
+                } catch (Exception ex)
+                {
+                    var logMessage = services.GetRequiredService<ILogger<Program>>();
+                    logMessage.LogError(ex, "Oops, something went wrong when we tried to seed the database.  Let the devs know so they can try to figure it out.");
+                }
+
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
