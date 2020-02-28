ea6bb7f8841f2699f597e9ecb0f8252a2209e085
Trevor <trevorschulz@users.noreply.github.com>
2020-02-27 20:48:30 -0700

    Added Rachels code for the sort, and made the code campatible.

--- MegaDesk/Pages/Desks/Index.cshtml.cs
+++ MegaDesk/Pages/Desks/Index.cshtml.cs
@@ -20,24 +20,55 @@ namespace MegaDesk
             _context = context;
         }
 
-        public IList<Order> Order { get;set; }
+        public IList<Order> Order { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         // Requires using Microsoft.AspNetCore.Mvc.Rendering;
-        public SelectList LastName { get; set; }
-        [BindProperty(SupportsGet = true)]
-        public string OrderSurfaceMaterial { get; set; }
 
-        public async Task OnGetAsync()
+
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public async Task OnGetAsync(string sortOrder)
         {
             var orders = from o in _context.Order
                          select o;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 orders = orders.Where(s => s.FirstName.Contains(SearchString) || s.LastName.Contains(SearchString));
+                Order = await orders.ToListAsync();
+
+
             }
 
-            Order = await orders.ToListAsync();
+            else
+            {
+                NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+                IQueryable<Order> deskQuote = from s in _context.Order
+                                              select s;
+
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        deskQuote = deskQuote.OrderByDescending(s => s.LastName);
+                        break;
+                    case "Date":
+                        deskQuote = deskQuote.OrderBy(s => s.DateAdded);
+                        break;
+                    case "date_desc":
+                        deskQuote = deskQuote.OrderByDescending(s => s.DateAdded);
+                        break;
+                    default:
+                        deskQuote = deskQuote.OrderBy(s => s.LastName);
+                        break;
+                }
+
+
+
+                Order = await deskQuote.ToListAsync();
+
+            }
         }
     }
 }
